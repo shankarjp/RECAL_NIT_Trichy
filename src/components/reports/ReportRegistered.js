b659e2f3bb57a690d1038a92bd5df41e7fcf3fd0
vendhan-ds <104243394+vendhan-ds@users.noreply.github.com>
2022-10-06 20:22:53 +0530

    Update ReportRegistered.js

--- src/components/reports/ReportRegistered.js
+++ src/components/reports/ReportRegistered.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom'
 
 function ReportRegistered() {
 
-    const [user,setuser]=useState(null)
+    const [user,setuser]=useState([{username:'',pax:{spouse:0,familyme:0,grandKids:0}}])
     //const [paxdata, setpaxdata]=useState(null)
-    useEffect(()=>{
+    /* useEffect(()=>{
          fetch("http://localhost:8080/api/registered")
     .then((response)=>console.log(response))
     .then((response) => response.json())
@@ -14,7 +14,15 @@ function ReportRegistered() {
         setuser(response);
         //setpaxdata(response.pax);
     })
-    },[])
+    },[]) */
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/registered").then((res) => {
+        res = res.data;
+        console.log(res);
+        setuser(res);
+        console.log(user)
+        })
+    }, []);
 
     return (
         <div className='mainc'>
@@ -36,7 +44,7 @@ function ReportRegistered() {
                         <td>{data.pax.spouse}</td>
                         <td>{data.pax.familyMembers}</td>
                         <td>{data.pax.grandKids}</td>
-                        <td>{data.pax.spouse+data.pax.grandKids+data.pax.familyMembers+1}</td>
+                        <td>{data.pax.spouse+data.pax.grandKids+data.pax.familyMembers}+1</td>
                     </tr>
                 ))}
             </tbody>
